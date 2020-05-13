dca3461763f642220ec92d031e3226d1d0dd3beb
jihyuno301 <joh755807@gmail.com>
2020-05-13 11:52:04 +0900

    router, binding, ajax

--- expressServer.js
+++ expressServer.js
@@ -1,12 +1,47 @@
 const express = require('express')
 const app = express()
- 
+const path = require('path')
+
+app.set('views', path.join(__dirname, 'views')); // ejs file location
+app.set('view engine', 'ejs'); //select view templet engine
+
+app.use(express.static(path.join(__dirname, 'public')));//to use static asset
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 app.get('/', function (req, res) {
-    res.send('Hello Express')
+    var title = "javascript"
+    res.send('<html><h1>'+title+'</h1><h2>contents</h2></html>')
+})
+
+app.get('/ejs', function(req, res){
+    res.render('test')
 })
 
 app.get('/test', function(req, res){
     res.send('Test')
+})
+
+app.get('/design', function(req, res){
+    res.render('designTest');
+})
+
+//datasend Router add
+app.get('/dataSend', function(req, res){
+    res.render('dataSend');
+})
+
+app.post('/getTime', function(req, res){
+    var nowTime = new Date();
+    res.json(nowTime);
+})
+
+app.post('/getData', function(req, res){
+    console.log(req.body);
+    var userData = req.body.userInputData;
+    console.log('userData = ', userData);
+    res.json(userData + "!!!!!")
 })
 
 app.listen(3000)
